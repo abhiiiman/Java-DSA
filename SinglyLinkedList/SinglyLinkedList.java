57083b4b70a588dbbc24e4451f466c8527bf1bda
Abhijit <abhijitmandal.btech21@sushantuniversity.edu.in>
2023-06-06 13:24:15 +0530

    Add Data

--- SinglyLinkedList/SinglyLinkedList.java
+++ SinglyLinkedList/SinglyLinkedList.java
@@ -14,4 +14,19 @@ public class SinglyLinkedList {
         }
     }
 
+    public static void main(String[] args) {
+
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10); // head data.
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        // now we have to connect them together.
+
+        sll.head.next = second; // 10-->1-->N
+        second.next = third; // 10-->1-->8-->N
+        third.next = fourth; // 10-->1-->8-->11-->N
+    }
+
 }
