510719338c4674a39ff457c25d63c424d98d7b10
Abhijit <abhijitmandal.btech21@sushantuniversity.edu.in>
2023-06-07 16:25:24 +0530

    initial commit

---
+++ Basics/DecimalComparator.java
@@ -0,0 +1,27 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class DecimalComparator {
+    public boolean areEqualByThreeDecimalPlaces(double num1, double num2){
+        int roundedNum1 = (int) Math.round(num1);
+        int roundedNum2 = (int) Math.round(num2);
+        if (roundedNum1 == roundedNum2){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the 1st number here:");
+        double number1 = input.nextDouble();
+        System.out.println("Enter the 2nd number here:");
+        double number2 = input.nextDouble();
+        input.close();
+
+        DecimalComparator checkDecimal = new DecimalComparator();
+        boolean result = checkDecimal.areEqualByThreeDecimalPlaces(number1, number2);
+        System.out.println(result);
+    }
+}
