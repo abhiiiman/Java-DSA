893b942a177fa754da2aba19ad2a218d321c82f6
Abhijit <abhijitmandal.btech21@sushantuniversity.edu.in>
2023-06-14 22:35:36 +0530

    added optimal solution code

--- Array/DupZeroes.java
+++ Array/DupZeroes.java
@@ -22,9 +22,39 @@ public class DupZeroes {
         return nums;
     }
 
+    public static void transfer(int[] arr, int i, int j) {
+        if (j < arr.length) {
+            arr[j] = arr[i];
+        }
+    }
+
+    public static int[] duplicate(int[] nums) {
+        int z = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                z++;
+            }
+        }
+        int i = n - 1;
+        int j = n + z - 1;
+        while (i != j) {
+            transfer(nums, i, j);
+            j--;
+            if (nums[i] == 0) {
+                transfer(nums, i, j);
+                j--;
+            }
+            i--;
+        }
+        return nums;
+    }
+
     public static void main(String[] args) {
         int arr[] = { 1, 0, 2, 0, 4, 0 };
-        System.out.println(duplicateZero(arr));
+        // int arr[] = { 8, 5, 6, 7, 0, 0, 0, 0, 7 };
+        // System.out.println(duplicateZero(arr));
+        duplicate(arr);
         printArray(arr);
     }
 }
